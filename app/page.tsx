bc97bf6ce9eadbd5376ef8bd820f1fd45eb4852a
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2025-09-19 07:12:46 -0400

    Added Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <h1 className="text-4xl font-bold text-red-500">
+      {/* <h1 className="text-4xl font-bold text-red-500">
       Tailwind is finally working!
-    </h1>
+    </h1> */}
       <Hero />
     </main>
   );
