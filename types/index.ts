30af273acd8488796c66c8c23b03e5bf49e85ab7
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2025-09-23 06:54:13 -0400

    Added API Data fetching and CarCard

--- types/index.ts
+++ types/index.ts
@@ -4,10 +4,28 @@ export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: "button" | "submit"
+    btnType?: "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+city_mpg: string;
+class: string;
+combination_mpg: string;
+cylinders: number;
+displacement: number;
+drive: string;
+fuel_type: string;
+highway_mpg: string;
+make: string;
+model: string;
+transmission: string;
+year: number;
 }
