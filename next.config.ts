2f3c3a4232b36e0739244cba5a4509adf198793f
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2026-03-15 09:48:47 -0400

    Deleted page.tsx from admin subdir

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,4 @@
-// import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   images: {
-//     //domains: ['cdn.imagin.studio']
-//     domains: ['res.cloudinary.com']
-//   }
-// };
-
-// export default nextConfig;
-/************************** */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
